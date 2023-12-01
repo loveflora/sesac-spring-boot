b3eae7d45c4fde6ad222767f51fa043c0e4b2dae
saywhat <95282021+loveflora@users.noreply.github.com>
2023-12-01 18:11:27 +0900

    Initial commit

---
+++ src/main/java/sesac/sesacspringboot/vo/UserVO.java
@@ -0,0 +1,12 @@
+package sesac.sesacspringboot.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserVO {
+        private String name;
+        private String age;
+
+}
